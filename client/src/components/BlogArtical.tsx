1e46a821a509ccb802e8ad97b354176660a2f301
Ax Micky <attigmohammed@gmail.com>
2022-06-29 18:11:42 +0100

    changed import to constant require

--- client/src/components/BlogArtical.tsx
+++ client/src/components/BlogArtical.tsx
@@ -1,11 +1,11 @@
-import ProfileImage from "../static/images/me_pic3.jpg";
 import "../styles/blog_section.css";
+const ProfileImage = "../static/images/me_pic3.jpg";
 
 function BlogArtical() {
   return (
     <div className="blog-section">
       <div className="blog-artical">
-        <img src={ProfileImage} alt="" className="blog-image" />
+        <img src={require(ProfileImage)} alt="" className="blog-image" />
         <div className="blog-content">
           <h1 className="title-blog">Blog Title</h1>
           <p className="blog-text">
