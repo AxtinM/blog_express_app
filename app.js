e05e96c6fa94889de8a499f9100a1a2e85919ac8
Ax Micky <attigmohammed@gmail.com>
2022-06-29 17:11:16 +0100

    ready build

--- app.js
+++ app.js
@@ -11,15 +11,20 @@ let app = express();
 require("./models/db");
 
 // setup cors
-app.use(cors());
+// app.use(cors());
 app.use(logger("dev"));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
+app.set("port", process.env.PORT || 8000);
+
+app.use(express.static(path.resolve(__dirname, "client", "build")));
+
 app.use("/api", indexRoutes);
 
-app.listen(8000, function () {
-  console.log("Server running on port 8000");
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
 });
