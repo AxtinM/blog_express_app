b0587c1803e65a647369e94477517fe0acaae741
Ax Micky <attigmohammed@gmail.com>
2022-07-05 16:11:08 +0100

    app.js

--- app.js
+++ app.js
@@ -23,9 +23,6 @@ app.set("port", process.env.PORT || 8000);
 
 app.use("/api", indexRoutes);
 
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-});
 app.listen(app.get("port"), () => {
   console.log(`Server is running on port ${app.get("port")}`);
 });
