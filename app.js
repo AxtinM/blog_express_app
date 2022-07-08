c26097e605c7dd927453def9729a43c8ff163cd9
Ax Micky <attigmohammed@gmail.com>
2022-07-08 19:24:52 +0100

    updated app.js to work in production

--- app.js
+++ app.js
@@ -23,6 +23,10 @@ app.set("port", process.env.PORT || 8000);
 
 app.use("/api", indexRoutes);
 
+if (process.env.NODE_ENV == "production") {
+  app.use(express.static("./client/build"));
+}
+
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "client", "build", "index.html"));
 });
